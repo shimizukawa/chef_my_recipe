9b3d6b6693de0805b58d5fa5b67aaa759f7d90a7
shimizukawa <shimizukawa@gmail.com>
2013-01-17 10:24:18 +0900

    add screenrc

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@ require_recipe "apt"
 package "curl"
 package "screen"
 package "subversion"
+
+cookbook_file "~/.screenrc" do
+  source 'screenrc.dot'
+  action :create_if_missing
+  owner owner
+  mode "0664"
+end
